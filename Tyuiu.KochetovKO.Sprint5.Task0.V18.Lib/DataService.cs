11028599f968a6608e0099c3ab0018f3a6bc1129
Кирилл Кочетов <Tyuiu.KochetovKO@outlook.com>
2025-11-22 17:56:50 +0500

    Task0V18

--- Tyuiu.KochetovKO.Sprint5.Task0.V18.Lib/DataService.cs
+++ Tyuiu.KochetovKO.Sprint5.Task0.V18.Lib/DataService.cs
@@ -1,5 +1,7 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using tyuiu.cources.programming.interfaces.Sprint5;
+
 namespace Tyuiu.KochetovKO.Sprint5.Task0.V18.Lib
 {
     public class DataService : ISprint5Task0V18
@@ -7,10 +9,10 @@ namespace Tyuiu.KochetovKO.Sprint5.Task0.V18.Lib
         public string SaveToFileTextData(int x)
         {
             string path = $@"{Directory.GetCurrentDirectory()}\OutPutFileTask0.txt";
-            double y = x / (Math.Sqrt(x * x + x));
+            double y = (double)x / (Math.Sqrt(x * x + x));
             y = Math.Round(y, 3);
             File.WriteAllText(path, Convert.ToString(y));
-            return path; 
+            return path;
         }
     }
 }
