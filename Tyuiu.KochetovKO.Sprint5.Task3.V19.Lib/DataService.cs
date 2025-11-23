aa0d4c4a03e775d74a16b2911abe5c90fead9681
Кирилл Кочетов <Tyuiu.KochetovKO@outlook.com>
2025-11-23 22:32:45 +0500

    Task3V19

--- Tyuiu.KochetovKO.Sprint5.Task3.V19.Lib/DataService.cs
+++ Tyuiu.KochetovKO.Sprint5.Task3.V19.Lib/DataService.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Text;
 using tyuiu.cources.programming.interfaces.Sprint5;
 namespace Tyuiu.KochetovKO.Sprint5.Task3.V19.Lib
@@ -21,27 +22,6 @@ namespace Tyuiu.KochetovKO.Sprint5.Task3.V19.Lib
                 writer.Write(BitConverter.GetBytes(y));
             }
 
-            return path;
-        }
-    }
-}
-using System.Text;
-using tyuiu.cources.programming.interfaces.Sprint5;
-namespace Tyuiu.KhisamutdinovaPR.Sprint5.Task3.V30.Lib
-{
-    public class DataService : ISprint5Task3V30
-
-    {
-        public string SaveToFileTextData(int x)
-        {
-            string path = Path.GetTempFileName();
-
-            double y = Math.Round((Math.Pow(x, 3) - 1) / (4 * x * x), 3);
-
-            using (BinaryWriter writer = new BinaryWriter(File.Open(path, FileMode.OpenOrCreate), Encoding.UTF8))
-            {
-                writer.Write(BitConverter.GetBytes(y));
-            }
             return path;
         }
     }
