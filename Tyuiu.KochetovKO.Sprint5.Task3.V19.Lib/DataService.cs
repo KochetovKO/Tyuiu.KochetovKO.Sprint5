827aff5f179d21e8007c06551d93fedb9cf36844
Кирилл Кочетов <Tyuiu.KochetovKO@outlook.com>
2025-11-23 22:41:14 +0500

    Task3V19

--- Tyuiu.KochetovKO.Sprint5.Task3.V19.Lib/DataService.cs
+++ Tyuiu.KochetovKO.Sprint5.Task3.V19.Lib/DataService.cs
@@ -9,19 +9,20 @@ namespace Tyuiu.KochetovKO.Sprint5.Task3.V19.Lib
         public string SaveToFileTextData(int x)
         {
 
-            string path = Path.ChangeExtension(Path.GetTempFileName(), ".bin"); 
-            
-            double y;
-           
-            y = (2 * x * x - 1) / Math.Sqrt(x * x - 2);
-
-            y = Math.Round(y, 3);
-
-            using (BinaryWriter writer = new BinaryWriter(File.Open(path, FileMode.OpenOrCreate), Encoding.UTF8))
+            string path = Path.Combine(Path.GetTempPath(), "OutPutFileTask3.bin");
+            FileInfo FileInfo = new FileInfo(path);
+            bool fileExists = FileInfo.Exists;
+            if (fileExists)
             {
-                writer.Write(BitConverter.GetBytes(y));
+                File.Delete(path);
             }
-
+            double y = (2 * Math.Pow(x, 2) - 1) / Math.Sqrt(Math.Pow(x, 2) - 2);
+            y = Math.Round(y, 3);
+            using (BinaryWriter writer = new BinaryWriter(File
+                    .Open(path, FileMode.Create)))
+            {
+                writer.Write(y);
+            }
             return path;
         }
     }
