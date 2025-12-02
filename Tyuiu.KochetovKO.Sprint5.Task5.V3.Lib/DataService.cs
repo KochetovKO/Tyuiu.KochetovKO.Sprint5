a878d132c00aceaaea8b7ab8e01bc4ae2a34a0ed
Кирилл Кочетов <Tyuiu.KochetovKO@outlook.com>
2025-12-02 20:35:52 +0500

    Task5V3

--- Tyuiu.KochetovKO.Sprint5.Task5.V3.Lib/DataService.cs
+++ Tyuiu.KochetovKO.Sprint5.Task5.V3.Lib/DataService.cs
@@ -7,7 +7,7 @@ namespace Tyuiu.KochetovKO.Sprint5.Task5.V3.Lib
 {
     public class DataService : ISprint5Task5V3
     {
-        public double LoadFromDataFile(string path)
+        public double LoadFromDataFile(string path) 
         {
             double res = 0;
             using (StreamReader reader = new StreamReader(path))
