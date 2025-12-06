eb5991936e31aabb04cfc9b61865dd192ebfb25f
Кирилл Кочетов <Tyuiu.KochetovKO@outlook.com>
2025-12-06 16:20:41 +0500

    Task6V24

---
+++ Tyuiu.KochetovKO.Sprint5.Task6.V24.Test/DataServiceTest.cs
@@ -0,0 +1,12 @@
+﻿using Tyuiu.KochetovKO.Sprint5.Task6.V24.Lib;
+namespace Tyuiu.KochetovKO.Sprint5.Task6.V24.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Tyuiu.KochetovKO.Sprint5.Task6.V24/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.IO;
+using System;
+using Tyuiu.KochetovKO.Sprint5.Task6.V24.Lib;
+
+namespace Tyuiu.KochetovKO.Sprint5.Task6.V24
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #5 | Выполнил: Кочетов К. О. | ИCПб-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Тема: Класс File. Запись набора данных в текстовый файл                *");
+            Console.WriteLine("* Задание #6                                                              *");
+            Console.WriteLine("* Вариант #24                                                              *");
+            Console.WriteLine("* Выполнил: Кочентов Кирилл Олегович | ИCПб-25-1                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            string path = @"C:\DataSprint5\InPutDataFileTask6V24.txt";
+
+            Console.WriteLine("Данные находятся в файле" + path);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            double res = ds.LoadFromDataFile(path);
+            Console.WriteLine(res);
+            Console.ReadKey();
+
+        }
+    }
+}
