d40ffd67b62272267b49e4a29cbefa965da33655
Кирилл Кочетов <Tyuiu.KochetovKO@outlook.com>
2025-12-06 17:00:20 +0500

    Task7V6

--- Tyuiu.KochetovKO.Sprint5.Task7.V6.Lib/DataService.cs
+++ Tyuiu.KochetovKO.Sprint5.Task7.V6.Lib/DataService.cs
@@ -4,7 +4,7 @@ using System.Text;
 using tyuiu.cources.programming.interfaces.Sprint5;
 namespace Tyuiu.KochetovKO.Sprint5.Task7.V6.Lib
 {
-    public class DataService : ISprint5Task7V6
+    public class DataService : ISprint5Task7V6 
     {
         public string LoadDataAndSave(string path)
         {
